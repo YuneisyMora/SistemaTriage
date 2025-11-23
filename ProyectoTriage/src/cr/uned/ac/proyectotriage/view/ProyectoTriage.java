d6104711fe2a1c59129df022c9cc1a0f668e5c2d
Yuneisy Mora <yunemora9@gmail.com>
2025-11-22 22:30:58 -0600

    vista principal

--- ProyectoTriage/src/cr/uned/ac/proyectotriage/view/ProyectoTriage.java
+++ ProyectoTriage/src/cr/uned/ac/proyectotriage/view/ProyectoTriage.java
@@ -2,11 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package proyectotriage;
+package cr.uned.ac.proyectotriage.view;
 
 /**
  *
- * @author micha
  */
 public class ProyectoTriage {
 
